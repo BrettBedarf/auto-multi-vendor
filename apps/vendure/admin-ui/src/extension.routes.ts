28b0c33dd5ff80ac7c36cf67339c5d59d3f73a5d
brett <brettbedarf.dev@gmail.com>
2022-06-10 13:31:53 -0600

    (vendure) remove admin-ui generated code from git

--- apps/vendure/admin-ui/src/extension.routes.ts
+++
@@ -1,4 +0,0 @@
-export const extensionRoutes = [  {
-    path: 'extensions/examples',
-    loadChildren: () => import('./extensions/97d6d2c504c70088b265b6389b0fea09d61f088eb60152347782182eda180ca8/example-ui-lazy.module').then(m => m.ExampleUiLazyModule),
-  }];
