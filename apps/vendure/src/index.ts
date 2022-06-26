67b9ac5ed77ed77bbbfe8e99f606281ec76addd5
brett <brettbedarf.dev@gmail.com>
2022-06-25 23:23:08 -0600

    (vendure) always try to load from env file
    minimal performance impact because only run at startup and was causing issues forgetting to run with yarn dev vs yarn start

--- apps/vendure/src/index.ts
+++ apps/vendure/src/index.ts
@@ -1,13 +1,15 @@
-// use .env file in dev mode only (production should be injected by platform)
-if (
-	process.env.NODE_ENV?.toLowerCase() === 'dev' ||
-	process.env.NODE_ENV?.toLowerCase() === 'development'
-) {
-	import('dotenv/config');
-}
+// // use .env file in dev mode only (production should be injected by platform)
+// if (
+// 	process.env.NODE_ENV?.toLowerCase() === 'dev' ||
+// 	process.env.NODE_ENV?.toLowerCase() === 'development'
+// ) {
+// 	import('dotenv/config');
+// }
+
+import('dotenv/config');
+
 import { bootstrap } from '@vendure/core';
 import { config } from './vendure-config';
-
 
 bootstrap(config).catch(err => {
 	// tslint:disable-next-line:no-console
